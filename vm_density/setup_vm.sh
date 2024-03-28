336237fabdf8d05130128c65e02c2f18b0ce6302
Xinyi He <xinyi.he@intel.com>
2024-03-28 16:25:47 +0800

    Update setup_vm.sh

--- vm_density/setup_vm.sh
+++ vm_density/setup_vm.sh
@@ -2,3 +2,17 @@ dnf install @virt
 systemctl start libvirtd
 systemctl enable libvirtd
 dnf install virt-install
+
+
+virt-install \
+--name=centos7-vm \
+--ram=8192 \
+--vcpus=8 \
+--disk path=/var/images/centos7-vm.img,size=40 \
+--os-type=linux \
+--os-variant=centos8 \
+--network bridge=virbr0 \
+--graphics none \
+--console pty,target_type=serial \
+--location=//var/iso/CentOS-7-x86_64-Minimal-2207-02.iso \
+--extra-args='console=ttyS0,115200n8 serial'
