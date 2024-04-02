f9f7e6e8d4b47a6c500969bb9e07858f87b2a76a
Xinyi He <xinyi.he@intel.com>
2024-04-02 15:56:25 +0800

    Update setup_vm.sh

--- vm_density/setup_vm.sh
+++ vm_density/setup_vm.sh
@@ -1,3 +1,16 @@
+dnf groupinstall "Server with GUI"
+systemctl set-default graphical
+dnf install tigervnc-server tigervnc-server-module -y
+vim /etc/tigervnc/vncserver.users
+:1=root
+systemctl daemon-reload
+vncpasswd
+systemctl start vncserver@:1.service
+systemctl enable vncserver@:1.service
+systemctl status vncserver@:1.service
+
+
+
 dnf install @virt
 systemctl start libvirtd
 systemctl enable libvirtd
@@ -11,8 +24,13 @@ virt-install \
 --disk path=/var/images/centos7-vm.img,size=40 \
 --os-type=linux \
 --os-variant=centos8 \
---network bridge=virbr0 \
+--network bridge=virbr0,model=virtio \
 --graphics none \
 --console pty,target_type=serial \
---location=//var/iso/CentOS-7-x86_64-Minimal-2207-02.iso \
+--location=/var/iso/CentOS-7-x86_64-Minimal-2207-02.iso \
 --extra-args='console=ttyS0,115200n8 serial'
+
+qemu-img create -f qcow2 -F qcow2  -b /var/lib/libvirt/images/centos7.0.qcow2 /var/images/centos7.qcow2
+
+virsh domifaddr 
+virsh domblklist
